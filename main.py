eba3f366ff292b367b61dddafd1f1a80475c3b4f
Kevin TSI <kevtsiii@gmail.com>
2021-12-13 15:39:36 +0100

    Add some comments

--- main.py
+++ main.py
@@ -2,26 +2,25 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+# Get code source Championnat de france de football page
 request_text = requests.get("https://fr.wikipedia.org/wiki/Championnat_de_France_de_football_2019-2020")
 
-print(request_text)
+#print(request_text)
 
+# Instance BeautifulSoup and parse the content of request_text
 soup = BeautifulSoup(request_text.content, 'html.parser')
 
 #print(soup.prettify())
 
 print("Il y a {} balises table ".format(len(soup.find_all("table"))))
 
-#print(soup.find("table"))
-
-#print(soup.find_all("table",class_="wikitable", limit=1))
-
+# Find the table with class DebutCarte
 table = soup.find("table",class_="DebutCarte")
 
-#print(table.find("tbody").find_all("a"))
-
+# Get the first five <a> elements
 clubs = table.find_all("a")[:5]
 
+# Create dataframe
 df = pd.DataFrame(columns=["Club","URL"])
 
 for club in clubs:
@@ -33,5 +32,7 @@ for club in clubs:
     print("----------")
 
 print(df)
+
+# Save to csv and xls
 df.to_csv("./Club_Web_Scraping.csv")
-df.to_excel("./Club_Web_Scraping.xlsx")
+df.to_excel("./Club_Web_Scraping.xls")
